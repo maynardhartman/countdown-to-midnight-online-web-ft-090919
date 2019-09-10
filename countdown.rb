f19ceac9e74db13374e9ed2d54d9858bd0969eaf
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-10 17:22:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 
 def countdown(number)
-  while number > 10
+  while number > 0
   do
     puts "#{number} SECOND(S)!"
     number -= 1
     sleep(1)
   end
+  return "HAPPY NEW YEAR!"
 end
